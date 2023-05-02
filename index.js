0b4cfeeceaf3528df5ca518289012b568777db15
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-02 13:32:07 +0600

    cors installed & added...

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const cors = require("cors");
+
 const port = process.env.PORT || 5000;
 
 const chefDetails = require("./data/randomData.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("welcome to Thai Masala Server");
