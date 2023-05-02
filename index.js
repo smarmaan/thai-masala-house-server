781fc90328cedb47619416356c757833b621d164
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-02 13:26:15 +0600

    random data added..  in order to test server...

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const chefDetails = require("./data/randomData.json");
+
 app.get("/", (req, res) => {
   res.send("welcome to Thai Masala Server");
+});
+
+app.get("/chef-details", (req, res) => {
+  res.send(chefDetails);
 });
 
 app.listen(port, () => {
