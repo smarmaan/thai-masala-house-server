364e892fe53455d93cd187e16426d94ebfb45db7
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-02 23:32:54 +0600

    different chefs recipe list data created...

--- index.js
+++ index.js
@@ -16,12 +16,30 @@ app.get("/", (req, res) => {
   res.send("welcome to Thai Masala Server");
 });
 
+app.get("/chef-details", (req, res) => {
+  res.send(chefDetails);
+});
+
+app.get("/chef-details/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const chefRecipesList = recipes.filter((recipe) => recipe.chef_id === id);
+
+  res.send(chefRecipesList);
+});
+
 app.get("/recipes", (req, res) => {
   res.send(recipes);
 });
 
-app.get("/chef-details", (req, res) => {
-  res.send(chefDetails);
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+
+  console.log(id);
+
+  const selectedRecipes = recipes.find((recipe) => recipe.id === id);
+
+  res.send(selectedRecipes);
 });
 
 app.listen(port, () => {
